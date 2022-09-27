b54de755bbec54b9928f7994d5647c49cc8dd84e
anaparastasi <137augusto@gmail.com>
2022-09-27 20:32:59 -0300

    desafio

---
+++ desafio I/desafio01.js
@@ -0,0 +1,11 @@
+function teste(num){
+    let soma = 0{
+        num.forEach(element => {
+            
+        });
+    }
+}
+
+console.log(teste(10,9,6,8,9,1,5,7)) //6.875
+console.log(teste(10,10,10,9)) // 9.8
+console.log(teste(2,5,7,1,-2))// 2.6
